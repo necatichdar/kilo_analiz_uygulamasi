f97a4be24eaefee7d75e005a411fb710d8ca6fda
Necati Çuhadar <necaticuhadar06@gmail.com>
2020-11-04 17:37:12 +0300

    Provider eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:kilo_analiz_uygulamasi/yonlendirme.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:kilo_analiz_uygulamasi/services/yetkilendirme_servisi.dart';
+import 'package:kilo_analiz_uygulamasi/yonlendirme.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,13 +13,34 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (context) => YetkilendirmeServisi(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Yonlendirme(),
       ),
-      home: Yonlendirme(),
     );
   }
 }
+
+/*
+Provider<YetkilendirmeServisi>(
+      create: (context) => YetkilendirmeServisi(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Yonlendirme(),
+      ),
+    );
+ */
